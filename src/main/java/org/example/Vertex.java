7cffe42fd89c70566f7b1ee9c8ca91813626da5f
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2024-06-02 17:57:10 +0500

    mm

--- src/main/java/org/example/Vertex.java
+++ src/main/java/org/example/Vertex.java
@@ -2,24 +2,43 @@ package org.example;
 
 import java.util.*;
 
-public class Vertex<V> {
-    private final V data;
-    private final Map<Vertex<V>, Double> adjacentVertices; //this is with weights
+public class Vertex<T> {
+    private T data;
+    private Map<Vertex<T>, Double> adjacentVertices; //this is with weights
 
-    public Vertex(V data) {
+    public Vertex(T data) {
         this.data = data;
         this.adjacentVertices = new HashMap<>();
     }
 
-    public V getData() {
+    //getters&setters
+    public T getData() {
         return data;
     }
-
-    public void addAdjacentVertex(Vertex<V> destination, double weight) {
-        adjacentVertices.put(destination, weight);
+    public void setData(T data) {
+        this.data = data;
     }
 
-//    public Map<Vertex<V>, Double> getAdjacentVertices() {
-//        return adjacentVertices;
-//    }
+    public void addAdjacentVertex(Vertex<T> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+    public Map<Vertex<T>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+    public void setAdjacentVertices(Map<Vertex<T>, Double> adjacentVertices) {
+        this.adjacentVertices = adjacentVertices;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) obj;
+        return Objects.equals(data, vertex.data);
+    }
+
+    //uses the hash code of the field -  data
+    @Override
+    public int hashCode() {
+        return Objects.hash(data);
+    }
 }
